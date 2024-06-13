0c0a355ed1fecf642a981694287ca445706f676d
kuta <tt0387888@gmail.com>
2024-06-13 22:14:19 +0300

    added is_power_of_5 func

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from utils import factorial
 
 print(factorial(5))
 
+
+from utils import is_power_of_5
+
+print(is_power_of_5(25))
+
--- utils.py
+++ utils.py
@@ -14,3 +14,12 @@ def is_prime(n):
             return False
     return True
 
+
+
+def is_power_of_5(n):
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
+
