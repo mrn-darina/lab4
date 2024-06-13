5d146bfea19f4a4af3329cd08dc5fbb72db08359
kuta <tt0387888@gmail.com>
2024-06-13 22:11:07 +0300

    added .py files

---
+++ utils.py
@@ -0,0 +1,16 @@
+import math
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
